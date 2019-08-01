414c106529fb84478bb6db2c58c9a92eae709c10
hady <hady.sane@epitech.eu>
2019-08-01 17:47:48 +0200

    Mise en place le map et la liste de poi
    Mise en place du map et la liste de poi

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Map from './components/Map';
+import ListPoi from './components/ListPoi.js';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppNavigator = createStackNavigator({
+    Map: {
+      screen: Map
+    },
+    ListPoi: {
+        screen: ListPoi
+    },
+},{
+    initialRouteName:'Map',
 });
+  
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default class App extends React.Component
+{
+    constructor(props){
+        super(props); 
+        this.state = {
+            isLoading: true,
+            data:[], 
+        }
+    }
+
+    // componentDidMount(){
+    //     fetch('http://127.0.0.1:8000/users')
+    //     .then(function(res) {
+    //         this.setState({
+    //             isLoading:false,
+    //             data:res.data
+    //         })
+    //     })
+    // }
+
+    render(){
+        return <AppContainer />
+    }
+}
