147cf6d79b30eec6c920071f45c832b50723ae25
Julie <julie.jourdrouin@epitech.eu>
2019-08-02 15:04:08 +0200

    Change name branch

--- components/header/LogoHele.js
+++ components/header/LogoHele.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class LogoHele extends Component {
     render() {
     return (
         <View>
-            <Image style={{width: 50, height: 50}}
-            source={require('.././assets/logohele.png')} />
+        <Text >Header</Text>
         </View>
         );
     }
