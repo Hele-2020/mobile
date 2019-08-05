de41d85e2639a1dcba1ee396a75157a2e654caa2
hady <hady.sane@epitech.eu>
2019-08-05 11:36:10 +0200

    Mise en place de fetch
    fetch, state, recupération des pois

--- components/ListPoi.js
+++ components/ListPoi.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text,View } from 'react-native'; 
+import { FlatList, StyleSheet, Text,View, Dimensions } from 'react-native'; 
 
+const { width: winWidth } = Dimensions.get('window');
 export default class ListPoi extends React.Component
 {
     constructor(props){
         super(props); 
         this.state = {
             isLoading: true,
-            data:[], 
+            data:[],
         }
+    }
+
+       componentDidMount(){
+        fetch('http://f7b45ce9.ngrok.io/poi',{
+            method: 'GET',
+        })
+        .then((response) => response.json())
+        .then((poi) => {
+            this.setState({ data: poi })
+            console.log(poi)
+        })
+        .done();
+   
     }
 
    render(){
        return(
               <View style={styles.container}>
-                <FlatList style={ {width: 100 }}
-                    data={[
-                        {key: 'Devin'},
-                        {key: 'Jackson'},
-                        {key: 'James'},
-                        {key: 'Joel'},
-                        {key: 'John'},
-                        {key: 'Jillian'},
-                        {key: 'Jimmy'},
-                        {key: 'Julie'},
-                    ]}
-                renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+                <FlatList style={ {width:  winWidth }}
+                    data={this.state.data}
+                renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+                keyExtractor={(item, index) => index.toString()}
+                />
            </View>
        );
    }
@@ -34,12 +41,12 @@ export default class ListPoi extends React.Component
 const styles = StyleSheet.create({
     container: {
      flex: 1,
-     paddingTop: 22
+     paddingTop: 22,
     },
     item: {
       padding: 10,
       fontSize: 18,
-      height: 44,
+      height: 44
     },
   })
   
