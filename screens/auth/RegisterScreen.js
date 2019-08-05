a5471d53410f37b570d660c1ba645560f4522f43
LeNiglo <lefrantguillaume@gmail.com>
2019-08-05 18:42:13 +0200

    Added password alert and copy to clipboard

--- screens/auth/RegisterScreen.js
+++ screens/auth/RegisterScreen.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
-import { AsyncStorage, StyleSheet, TextInput, Button, View, KeyboardAvoidingView } from 'react-native';
+import {
+    AsyncStorage,
+    Clipboard,
+    StyleSheet,
+    TextInput,
+    Button,
+    View,
+    KeyboardAvoidingView
+} from 'react-native';
 
 import axios from 'axios';
 
@@ -25,14 +33,14 @@ export default class RegisterScreen extends Component {
         return (
             <KeyboardAvoidingView style={{flex: 1, justifyContent: 'center', alignItems: 'stretch', margin: 20}} behavior='padding'>
 
-            <TextInput value={this.state.phone} onChangeText={text => this.setState({phone: text})} textContentType='telephoneNumber' placeholder="Phone" />
-            <TextInput value={this.state.username} onChangeText={text => this.setState({username: text})} textContentType='username' placeholder="Username" />
-            <TextInput value={this.state.age} onChangeText={text => this.setState({age: text})} placeholder="Age" />
-            <TextInput value={this.state.region} onChangeText={text => this.setState({region: text})} textContentType='addressState' placeholder="Region" />
+                <TextInput value={this.state.phone} onChangeText={text => this.setState({phone: text})} textContentType='telephoneNumber' placeholder="Phone" />
+                <TextInput value={this.state.username} onChangeText={text => this.setState({username: text})} textContentType='username' placeholder="Username" />
+                <TextInput value={this.state.age} onChangeText={text => this.setState({age: text})} placeholder="Age" />
+                <TextInput value={this.state.region} onChangeText={text => this.setState({region: text})} textContentType='addressState' placeholder="Region" />
 
-            <Button title="Register !" onPress={this._registerAsync} />
-            <Button title="Login" onPress={this._redirectLogin} />
-            
+                <Button title="Register !" onPress={this._registerAsync} />
+                <Button title="Login" onPress={this._redirectLogin} />
+
             </KeyboardAvoidingView>
         );
     };
@@ -41,8 +49,14 @@ export default class RegisterScreen extends Component {
         axios.post(Api.url('/auth/register'), this.state)
         .then(async response => {
             console.log(response);
-
-
+            if (response.data.password) {
+                await Clipboard.setString(response.data.password)
+                alert(
+                    "Mot de passe copié dans le presse papier:\n\n" +
+                    response.data.password
+                )
+            }
+            this.props.navigation.navigate('Login');
         })
         .catch(error => {
             console.log(error.response.data);
