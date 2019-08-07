1f64adba2cc8c87d962bdb3e8c59ea15134a3d57
myriam <myriam.azzimani@epitech.eu>
2019-08-07 10:51:05 +0200

    visuel de chat, websocket operationnel

--- SocketConnexion.js
+++ SocketConnexion.js
@@ -1,9 +1,13 @@
 import Ws from '@adonisjs/websocket-client'
 
-const ws = Ws('ws://0.tcp.ngrok.io:13858')
+const ws = Ws('ws://0.tcp.ngrok.io:14585')
 
-ws.connect()
+ws
+    .withJwtToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMsImlhdCI6MTU2NTAwOTgyNH0.gIcTxtRItxTSlyuJQ1UPDLUtR87j_mjiUYHqkMtHros')
+    .connect()
+
+
     
-const chat = ws.subscribe('chat')
+const chat = ws.subscribe('chat:*')
 
 export default chat;
