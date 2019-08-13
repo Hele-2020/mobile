1ab571ed5fceee49ba3f5cb8463b1f76f65e3c75
Julie <julie.jourdrouin@epitech.eu>
2019-08-13 16:10:49 +0200

    footer Commentaire ok

--- components/comment/footer/PostNewComment.js
+++ components/comment/footer/PostNewComment.js
@@ -26,10 +26,6 @@ export default class PostNewComment extends Component {
   };
 }
 const styles = StyleSheet.create({
-  form: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
   view: {
     flex: 0.6,
     alignContent: "space-around",
@@ -47,42 +43,21 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     backgroundColor: "#F1F0EF",
     color: "grey",
-    width: "60%",
-    paddingLeft: 10,
-    fontSize: 17,
+    width: "auto",
+    padding:"2%",
   },
   text: {
-    color: "white"
+    color: "white",
   },
   touchableButton: {
     backgroundColor: "#59358B",
     marginLeft: 10,
-    padding: 10,
+    marginBottom: 0,
+    marginTop: 0,
+    padding: "2%",
     width: "auto",
     color: "white",
     textDecorationColor: "white",
     borderRadius: 50,
   }
 });
-// export default class PostNewComment extends Component {
-//   constructor(props) {
-//       super(props);
-//       this.state = { text: '' };
-//     }
-    
-// render() {
-//   return (
-//     <View style={styles.view} >
-//     <KeyboardAvoidingView behavior="padding" style={styles.form}>
-//         <TextInput style={styles.textInput} placeholder="votre commentaire ... "
-//             onChangeText={(text) => this.setState({text})}
-//             value={this.state.text} autoCorrect={false} onSubmitEditing={this._submit}/>
-//           <View>
-//             <TouchableOpacity style={styles.touchableButton}>
-//                 <Button style={styles.button} onPress={ () => this._submit()} color="white" title="envoyer"/>
-//             </TouchableOpacity>
-//           </View>
-//        </KeyboardAvoidingView>
-//     </View>
-//   );
-// }
