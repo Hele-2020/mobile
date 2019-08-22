17e23225b0fff18b20ecf71dcdcec31dbe5baefb
gabriel <gabriel.emmanuel@epitech.eu>
2019-08-22 15:02:59 +0200

    RecupPostpro

--- components/SocketConn.js
+++ components/SocketConn.js
@@ -4,13 +4,13 @@ import Ws from '@adonisjs/websocket-client'
 
 //const token = AsyncStorage.getItem('userToken');
 const ws = Ws('ws://ba28864f.ngrok.io')
-ws.withJwtToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjYsImlhdCI6MTU2NjM5MjM3OH0.SFknx0HZBK0W-gmK27CsYW02V6VllXRiuP4mnNR0gHM").connect()
+ws.withJwtToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTU2NjQ2NTQyOH0.Lv9sKBISUsWYSdlEFxF9PacAeG0Tlz3F3NTzP1PBa2o").connect()
 const id_user = AsyncStorage.getItem('userId');
 // console.log("token: " + token)
 // console.log("id: " + id_user)
 
 const chat = ws.subscribe('post')
-// const post = ws.subscribe('postpsy')
+const post = ws.subscribe('postpsy')
 // const comment = ws.subscribe('comment')
 
 chat.on('error', (e) => {
