dfffb94c5d7934dc4255bf3a5eb2e0f0c5c84ae7
Julie <julie.jourdrouin@epitech.eu>
2019-08-26 14:21:18 +0200

    add fetch replies

--- components/comment/body/BodyComplete.js
+++ components/comment/body/BodyComplete.js
@@ -4,17 +4,35 @@ import { View, StyleSheet } from 'react-native';
 import BlocPost from '././infoPost/BlocPost.js';
 import BackgroundComment from './comments/BackgroundComment.js';
 // import comment from '../../SocketConn.js';
+// import Api from '../../../config/Api.js'
 export default class BodyComplete extends Component {
   constructor(props){
     super(props);
       this.state = {
-        messages: []
+        messages: [],
+        dataSource: []
       };
     }
   //   comment.on('send', (messageSock) => {
   //     this.setState({ messages: [...this.state.messages, messageSock] })
   //   })
   // }
+  componentDidMount(){
+    return fetch('http://d990dc07.ngrok.io/v1/replies')
+    .then((response) => response.json())
+    .then((responseJson) => {
+
+      this.setState({
+        dataSource: responseJson,
+      }, function(){
+
+      });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }
   render() {
     const NewComment = this.state.messages.map((message, key) => (
       <BackgroundComment key={key} message={message.message} date={message.date} name={message.name} />)
