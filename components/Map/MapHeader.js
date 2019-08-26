2e3553bd76f77f133555506523a4fc4916b2d475
hady <hady.sane@epitech.eu>
2019-08-26 13:55:42 +0200

    rassemblement mapview

--- components/Map/MapHeader.js
+++ components/Map/MapHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, TextInput, StyleSheet} from 'react-native';
+import {Text, View, TextInput, StyleSheet, AsyncStorage, ActivityIndicator} from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
 import Api from '../../config/Api';
 import axios from 'axios';
@@ -13,8 +13,8 @@ export default class MapHeader extends React.Component
             regions: [],
             selectedRegion: '',
             userRegionId: null
-            // TODO: userRegionId: this.props.user.region_id
         }
+        console.log(this.props.user)
     }
 
     componentDidMount = async () => {
@@ -26,13 +26,52 @@ export default class MapHeader extends React.Component
         .catch(error => {
             console.log(error.response.data);
         })
+        
+            const token = await AsyncStorage.getItem('userToken')
+            console.log(token)
+            const headers = {
+                'Authorization': 'bearer ' + token,
+            }
+    
+            axios.get(Api.url('/auth/me'), {headers : headers })
+            .then(async response => {
+                console.log(response.data.user)
+                this.setState({userRegionId: response.data.user.region_id})
+                console.log(this.state.userRegionId)
+    
+            })
+            .catch(error => {
+                console.log(error.response.data);
+            })
+        
     }
 
     render() {
+        if(this.state.userRegionId !== null) {
+             this.props.handleChange(this.state.userRegionId)
+            
+        } else {
+            return (
+                <View>
+                  <ActivityIndicator size="large" color="#0000ff" />
+                </View>
+            )
+        }
         return (
             <View style={{ borderBottomColor: "#FBBA00", borderBottomWidth : 1}}>
 
                 <ModalSelector
+                    optionTextStyle={styles.optionTextStyle}
+                    overlayStyle={styles.overlayStyle}
+                    optionContainer={styles.optionContainer}
+                    cancelContainer={styles.cancelContainer}
+                    selectStyle={styles.selectStyle}
+                    selectTextStyle={styles.selectTextStyle}
+                    optionStyle={styles.optionStyle}
+                    cancelStyle={styles.cancelStyle}
+                    sectionStyle={styles.sectionStyle}
+                    sectionTextStyle={styles.sectionTextStyle}
+                    initValueTextStyle={styles.initValueTextStyle}
                     data={this.state.regions}
                     cancelText={'Annuler'}
                     selectedKey={this.state.userRegionId}
@@ -56,7 +95,7 @@ export default class MapHeader extends React.Component
 
 const styles = StyleSheet.create({
     textInput: {
-        backgroundColor: 'rgba(0,0,0, .04)',
+        backgroundColor: 'rgba(0,0,0,.04)',
         borderWidth: 1,
         borderColor: '#DCDCDC',
         padding: 7,
@@ -67,7 +106,68 @@ const styles = StyleSheet.create({
         marginRight: 40,
         marginLeft: 40,
         fontSize: 15,
-        
-
+    }, overlayStyle: {
+        flex:            1,
+        padding:         '5%',
+        justifyContent:  'center',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+    },
+    optionContainer: {
+       // borderRadius:    BORDER_RADIUS,
+        flexShrink:      1,
+        marginBottom:    8,
+       // padding:         PADDING,
+        backgroundColor: 'rgba(255,255,255,0.8)',
+    },
+    cancelContainer: {
+        alignSelf: 'stretch',
+    },
+    selectStyle: {
+        borderColor:  '#ccc',
+        borderWidth:  1,
+       // padding:      PADDING,
+       // borderRadius: BORDER_RADIUS,
+    },
+    selectTextStyle: {
+        textAlign: 'center',
+        color:     '#333',
+        //fontSize:  FONT_SIZE,
+    },
+    cancelStyle: {
+       // borderRadius:    BORDER_RADIUS,
+        backgroundColor: '#FBBA00',
+       // padding:         PADDING,
+    },
+    cancelTextStyle: {
+        textAlign: 'center',
+        color:     '#333',
+       // fontSize:  FONT_SIZE,
+    },
+    optionStyle: {
+       // padding:           PADDING,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    optionTextStyle: {
+        textAlign: 'center',
+        //fontSize:  FONT_SIZE,
+        color:     '#59358B',
+    },
+    sectionStyle: {
+       // padding:           PADDING * 2,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        backgroundColor: '#59358B',
+    },
+    sectionTextStyle: {
+        textAlign: 'center',
+       // fontSize:  FONT_SIZE,
+        color:     'white',
+        //fontStyle: 'italic'
+    },
+    initValueTextStyle: {
+        textAlign: 'center',
+       // fontSize:  FONT_SIZE,
+        color:     '#d3d3d3'
     }
 })
