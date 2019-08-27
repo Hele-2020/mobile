b006dc279ba9856a2712577018ae1e6018731816
Julie <julie.jourdrouin@epitech.eu>
2019-08-27 16:01:45 +0200

    Maj  import Flatlist

--- components/post/body/BodyComplete.js
+++ components/post/body/BodyComplete.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import BlocPost from '././infoPost/BlocPost';
 //import post from '../../SocketConn.js';
 // import Api from '../../../config/Api.js'
