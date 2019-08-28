370692b34fcb54076c9901c1755e10354ff1bd69
myriam <myriam.azzimani@epitech.eu>
2019-08-28 17:37:25 +0200

    button create slot is hidden when the user is young

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,17 +3,35 @@ import { AsyncStorage, Button, View } from 'react-native';
 import ThreeAxisSensor from 'expo-sensors/build/ThreeAxisSensor';
 
 export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Roles:'',
+        }
+    }
     static navigationOptions = {
         title: 'Home',
     };
 
+    async componentDidMount() {
+
+        this.setState( { Roles : await AsyncStorage.getItem('userRoles')});
+        console.log(this.state.Roles)
+    }
+
+    displayButton() {
+        if (this.state.Roles === 'PROFESSIONAL') {
+            return  <Button title='Ouvrir un Créneau' onPress={this._SlotAsync} />
+        }
+    }
+
     render() {
         return (
             <View>
-                <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
-                <Button title='Ouvrir un Créneau' onPress={this._SlotAsync} />
+                <Button title="Actually, sign me out :)" onPress={this._signOutAsync} /> 
+                {this.displayButton()}
                 <Button title="Map" onPress={this._MapAsync} />
-                <Button title="Voir les créneaux" onPress={this._SlotSelectorAsync} />
+                <Button title="Voir les créneaux" onPress={this._SlotuserUserRolesuserUserRolesuserUserRolesuserUserRolesuserUserRolesuserUserRolesuserUserRolesuserUserRolesSelectorAsync} />
             </View>
         );
     }
